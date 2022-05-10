2cd1293a25136ba6ad7fb517a4281a38a57bcf55
javierpb275 <javierpb275@gmail.com>
2022-05-10 17:58:42 +0200

    matrix

--- src/com/javi/revision/arrays/Arrays.java
+++ src/com/javi/revision/arrays/Arrays.java
@@ -1,5 +1,7 @@
 package com.javi.revision.arrays;
 
+import java.util.Random;
+
 public class Arrays {
 
 	void myArrays() {
@@ -17,6 +19,37 @@ public class Arrays {
 
 		for (int b = 0; b < data2.length; b++) {
 			System.out.println(data2[b]);
+		}
+
+	}
+
+	void theMatrix() {
+
+		int[][] twoDimensionalArray = new int[10][10];
+		int[][][] threeDimensionalArray = new int[2][4][6];
+
+		Random rand = new Random(System.nanoTime());
+
+		for (int r = 0; r < twoDimensionalArray.length; r++) {
+
+			for (int c = 0; c < twoDimensionalArray[0].length; c++) {
+
+				twoDimensionalArray[r][c] = rand.nextInt(10);
+
+			}
+		}
+
+		for (int i = 0; i < twoDimensionalArray.length; i++) {
+
+			for (int j = 0; j < twoDimensionalArray[0].length; j++) {
+
+				System.out.print(twoDimensionalArray[i][j]);
+				System.out.print(" ");
+
+			}
+
+			System.out.println(" ");
+
 		}
 
 	}
