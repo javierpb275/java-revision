53c4c69efe66f8090516f45a0a08f4d345fda15e
javierpb275 <javierpb275@gmail.com>
2022-05-10 18:41:06 +0200

    oop

--- src/com/javi/revision/App.java
+++ src/com/javi/revision/App.java
@@ -1,9 +1,18 @@
 package com.javi.revision;
 
+import com.javi.revision.oop.Student;
+
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		
+		Student student1 = new Student();
+		
+		student1.id = "123456789-X";
+		student1.name = "Pepe";
+		
+		student1.showInfo();
 	}
 
 }
---
+++ src/com/javi/revision/oop/Student.java
@@ -0,0 +1,11 @@
+package com.javi.revision.oop;
+
+public class Student {
+	public String id;
+	public String name;
+	
+	public void showInfo() {
+		System.out.println("My name is: " + this.name);
+		System.out.println("My id is: " + this.id);
+	}
+}
