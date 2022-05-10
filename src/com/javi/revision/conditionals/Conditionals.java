25d8e442e9d9d6687a6f73339cfc9ea50c411398
javierpb275 <javierpb275@gmail.com>
2022-05-10 14:59:23 +0200

    conditionals: switch

--- src/com/javi/revision/conditionals/Conditionals.java
+++ src/com/javi/revision/conditionals/Conditionals.java
@@ -1,14 +1,14 @@
 package com.javi.revision.conditionals;
 
 public class Conditionals {
-	//
+
 	void getGrade() {
 		double grade = 4.6;
 		if (grade < 5 || grade == 0) {
 			System.out.println("F");
 		} else if (grade > 5 && grade < 6) {
 			System.out.println("Pass");
-		} else if (grade >=6 && grade < 7) {
+		} else if (grade >= 6 && grade < 7) {
 			System.out.println("Good");
 		} else if (grade >= 7 && grade < 9) {
 			System.out.println("Great");
@@ -16,4 +16,25 @@ public class Conditionals {
 			System.out.println("Outstanding!");
 		}
 	}
+
+	void checkNumber() {
+		int value = 1;
+
+		switch (value) {
+		case 1:
+			System.out.println("It's one");
+			break;
+		case 2:
+			System.out.println("It's two");
+			break;
+		case 3:
+			System.out.println("It's three");
+			break;
+		default:
+			System.out.println("It's neither one, nor two, nor three");
+			break;
+		}
+
+	}
+
 }
