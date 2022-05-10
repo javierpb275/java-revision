b5136cbac2749a27905a324f4d1d2f2d5887f63f
javierpb275 <javierpb275@gmail.com>
2022-05-10 15:20:06 +0200

    loops: while and do while

--- src/com/javi/revision/loops/Loops.java
+++ src/com/javi/revision/loops/Loops.java
@@ -2,8 +2,28 @@ package com.javi.revision.loops;
 
 public class Loops {
 	void forloop() {
-		for	(int a = 0; a < 10; a++) {
+		for (int a = 0; a < 10; a++) {
 			System.out.println(a);
 		}
+	}
+
+	void whileloop() {
+		int num = 10;
+
+		while (num != 0) {
+			System.out.println(num--);
+		}
+
+		// num-- : 10
+		// --num : 9
+	}
+
+	void doWhileloop() {
+		int num = 10;
+
+		do {
+			System.out.println(num--);
+		} while (num != 0);
+
 	}
 }
--- src/com/javi/revision/types/Types.java
+++ src/com/javi/revision/types/Types.java
@@ -1,31 +1,31 @@
 package com.javi.revision.types;
 
 public class Types {
-	//text:
+	// text:
 	String text = "Hi!";
 	char character = 'a';
-	
-	//numbers:
+
+	// numbers:
 	byte minByte = -128;
 	byte maxByte = 127;
-	
+
 	short minShort = -32768;
 	short maxShort = 32767;
-	
+
 	int minInt = -2147483648;
 	int maxInt = 2147483647;
-	
+
 	long minLong = -9223372036854775808L;
 	long maxLong = 9223372036854775807L;
-	
-	//decimal numbers:
+
+	// decimal numbers:
 	float minFloat = -3.402823e38F;
 	float maxFloat = 3.402823e38F;
-	
+
 	double minDouble = -1.79769313486232308D;
 	double maxDouble = 1.79769313486232308D;
-	
-	//boolean:
+
+	// boolean:
 	boolean theTruth = true;
 	boolean theLie = false;
 }
