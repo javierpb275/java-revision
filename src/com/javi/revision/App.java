bd147222d645ceea4d681e1e1c4ee7255797ec74
javierpb275 <javierpb275@gmail.com>
2022-05-10 13:48:15 +0200

    moved types to class Types

--- src/com/javi/revision/App.java
+++ src/com/javi/revision/App.java
@@ -4,33 +4,6 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		
-		//text:
-		String text = "Hi!";
-		char character = 'a';
-		
-		//numbers:
-		byte minByte = -128;
-		byte maxByte = 127;
-		
-		short minShort = -32768;
-		short maxShort = 32767;
-		
-		int minInt = -2147483648;
-		int maxInt = 2147483647;
-		
-		long minLong = -999999999;
-		long maxLong = 999999999;
-		
-		//decimal numbers:
-		float minFloat;
-		
-		double minDouble = -1.79769313486232308;
-		double maxDouble = 1.79769313486232308;
-		
-		//boolean:
-		boolean theTruth = true;
-		boolean theLie = false;
 	}
 
 }
